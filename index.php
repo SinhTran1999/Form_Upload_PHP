6cadbbc374d29173affce3a2c57802b5125be505
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-17 23:40:27 +0700

    add file index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    session_start();
+    include_once 'connect.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +10,35 @@
     <title>Form upload image</title>
 </head>
 <body>
-    <form action="upload.php" method ="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
+<?php 
+    if(isset($_SESSION['id'])){
+        if(isset($_SESSION['id']) == 1){
+           echo "You are logged in as user #1";
+        }
+        echo '<form action="upload.php" method ="post" enctype="multipart/form-data">
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <button type="submit" name="submit">UPLOAD</button>
+              </form>';
+    }else{
+        echo "You are not logged in!";
+        echo "<form action='login.php' method='POST'> 
+                <input type='text' name ='firstname' placeholder='First name'/>
+                <input type='text' name ='lastname' placeholder='Last name'/>
+                <input type='text' name ='username' placeholder='User name'/>
+                <input type='password' name ='password' placeholder='Password'/>
+                <button type='submit' name='submitSignUp'>Sign Up</button>
+              </form>";
+    }
+?>
+
+    <p>Login as user!</p>
+    <form action="login.php" method="POST">
+        <button type="submit" name="submitLogin">Login</button>
+    </form>
+
+    <p>Logout as user!</p>
+    <form action="logout.php" method="POST">
+        <button type="submit" name="submitLogout">Logout</button>
     </form>
 </body>
 </html>
