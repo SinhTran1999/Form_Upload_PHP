da0a654c9900fb97e7ebc6d9289749d510436523
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-17 22:17:22 +0700

    create table user in database ImageUpload on MySQL

--- connect.php
+++ connect.php
@@ -2,19 +2,26 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "ImageUpload";
     // Create connect
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
-
-    //Create database
-    $sql = "CREATE DATABASE ImageUpload"; 
+    // sql to create table
+    $sql = "CREATE TABLE user(
+        id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname varchar(50) NOT NULL,
+        lastname varchar(50) NOT NULL,
+        username varchar(256) NOT NULL,
+        password varchar(256) NOT NULL,
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
     if($conn->query($sql) === TRUE){
-        echo "Database created success fully";
+        echo "Table user created successfully";
     }else{
-        echo "Error creating database: ". $conn->error;
+        echo "Error creating table : ". $conn->error;
     }
     $conn ->close();
 ?>
